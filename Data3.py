1b90badef23e0dccba03d82978145aa5e3177fd4
zach <zachmonari002@gmail.com>
2025-10-31 09:00:06 +0300

    Basic data info

--- Data3.py
+++ Data3.py
@@ -1,4 +1,12 @@
 import pandas as pd
-
+#load the data
 df=pd.read_csv('student-data.csv')
+#display first 5 rows
 print(df.head())
+#data summary
+print(df.info())
+#statistical summary
+print(df.describe())
+#missing values
+print(df.isna())
+

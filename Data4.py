733f8423397263f52057557a32ec99783550dffb
zach <zachmonari002@gmail.com>
2025-11-02 09:17:51 +0300

    Loaded data for bar chart

--- Data4.py
+++ Data4.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
+
+#line plot
 x=[1,2,3,4,5]
 y=[2,4,6,8,10]
 
@@ -10,3 +14,24 @@ plt.xlabel('Days')
 plt.ylabel('Sales')
 plt.grid(alpha=0.3)
 plt.show()
+
+#bar chart
+df=pd.DataFrame({
+    'Name':["Zac","Kev","Dan","Ian","Jack"],
+    "Score":[96,80,75,85,90]
+})
+print(df.describe())
+print(df["Score"])
+print(df.info())
+print(df.shape)
+high_scores=df["Score"]>=85
+print(high_scores)
+sorted1=df.sort_values(by="Score",ascending=False)
+print(sorted1)
+
+df["Category"]=pd.cut(
+    df["Score"],
+    bins=[0,70,80,90,100],
+    labels=["D","C","B","A"]
+)
+print(df)

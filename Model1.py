264eb804e32bb20a0b56910ff3d6f2599a2c3b6a
zach <zachmonari002@gmail.com>
2025-11-06 08:40:30 +0300

    Added important libraries

---
+++ Model1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+

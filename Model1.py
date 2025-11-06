7045f2f8ba28511a6dc8c93a06d45dadf02beb83
zach <zachmonari002@gmail.com>
2025-11-06 09:31:07 +0300

    Added visualization libs

--- Model1.py
+++ Model1.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier

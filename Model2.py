009fba8ca9bfc0c231be084c7631045d5e002863
zach <zachmonari002@gmail.com>
2025-11-08 08:00:27 +0300

    Created model2 file

---
+++ Model2.py
@@ -0,0 +1,10 @@
+#important libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+#The dataset

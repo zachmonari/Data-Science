1602c3c48154e289f89a92e037ccded302726e41
zach <zachmonari002@gmail.com>
2025-11-08 08:58:27 +0300

    Did sanity checks

--- Model2.py
+++ Model2.py
@@ -11,3 +11,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 url = "https://raw.githubusercontent.com/rene-gith/water-potability/main/water_potability.csv"
 df = pd.read_csv(url)
 print("Rows, Cols:", df.shape)
+
+# Quick sanity checks
+print("Label counts:\n", df["Potability"].value_counts(dropna=False))
+print("\nMissing values per column:\n", df.isna().sum())

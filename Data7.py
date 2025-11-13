febb7b7ca21cc2c7f9faaa1e4eb4e2f9fcdd3992
zach <zachmonari002@gmail.com>
2025-11-13 07:43:14 +0300

    Loaded data

--- Data7.py
+++ Data7.py
@@ -3,3 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+
+# Sample data: Students' study time and grades
+data = {
+    'study_hours': [2, 3, 4, 8, 10, 12, 1, 6, 9, 11],
+    'grades': [40, 45, 50, 75, 80, 90, 35, 70, 85, 88]
+}
+
+df = pd.DataFrame(data)

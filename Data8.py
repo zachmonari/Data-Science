7f90bb644bb53dc04a8c049f2a567567d4aa9b9d
zach <zachmonari002@gmail.com>
2025-11-15 12:04:40 +0300

    Lib

--- Data8.py
+++ Data8.py
@@ -1,1 +1,6 @@
 #Libraries
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D

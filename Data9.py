6427883f6d81635026332b91ad39cb7420460805
zach <zachmonari002@gmail.com>
2025-11-17 09:05:28 +0300

    New student

--- Data9.py
+++ Data9.py
@@ -30,4 +30,8 @@ print("Accuracy:", accuracy_score(y_test, y_pred))
 print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
 print("Report:\n", classification_report(y_test, y_pred))
 
+new_student = [[6, 65]]
+prediction = model.predict(new_student)
+
+print("Pass (1) or Fail (0):", prediction)
 

b992860d8c306d12553cd5c7542815a563379ef9
zach <zachmonari002@gmail.com>
2025-11-17 09:02:34 +0300

    Trained model

--- Data9.py
+++ Data9.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 data = {
     "study_hours": [2, 3, 1, 5, 8, 7, 4, 9, 6, 10],
@@ -16,3 +17,7 @@ y = df["passed"]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+

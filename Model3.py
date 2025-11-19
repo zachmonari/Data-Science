0a61aff5822f785bc45d1f7f22c8f7625b5abc95
zach <zachmonari002@gmail.com>
2025-11-19 18:43:46 +0300

    Made predictions

--- Model3.py
+++ Model3.py
@@ -15,6 +15,11 @@ y = df["passed"]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
-
+# Train Decision Tree Classifier
 tree = DecisionTreeClassifier(max_depth=3, random_state=42)
 tree.fit(X_train, y_train)
+
+# make predictions
+y_pred = tree.predict(X_test)
+print("Predictions:", y_pred)
+

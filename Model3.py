dfcbf6ee92a23fae3198dac120ae9f3cf5560b52
zach <zachmonari002@gmail.com>
2025-11-19 18:38:37 +0300

    Loaded data

---
+++ Model3.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = {
+    "study_hours": [2, 3, 1, 5, 8, 7, 4, 9, 6, 10],
+    "previous_grade": [45, 50, 40, 60, 80, 78, 55, 90, 70, 95],
+    "passed": [0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+}
+
+df = pd.DataFrame(data)

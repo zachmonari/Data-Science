d5943bf65d2abd91a584e7c924ba05fe708d6010
zach <zachmonari002@gmail.com>
2025-11-19 18:40:16 +0300

    Training and testing data

--- Model3.py
+++ Model3.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 data = {
     "study_hours": [2, 3, 1, 5, 8, 7, 4, 9, 6, 10],
     "previous_grade": [45, 50, 40, 60, 80, 78, 55, 90, 70, 95],
@@ -7,3 +7,10 @@ data = {
 }
 
 df = pd.DataFrame(data)
+
+X = df[["study_hours", "previous_grade"]]
+y = df["passed"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
